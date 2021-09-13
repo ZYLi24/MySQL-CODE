dba160dbcb22eff7d58ad39f045068458c5b073a
Zhenyuan <zhenyuan.li524@gmail.com>
2021-09-13 22:41:40 +0930

    Update LeetCode All.sql

--- LeetCode All.sql
+++ LeetCode All.sql
@@ -114,3 +114,78 @@ FROM(
     ON e.DepartmentId = d.Id
 ) salary_rank
 WHERE ranking < 4
+
+### 196. Delete Duplicate Emails
+
+DELETE p2
+FROM Person p1, Person p2
+WHERE p1.Email = p2.Email AND p1.Id < p2.Id
+
+197. Rising Temperature
+
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY) AND w1.Temperature > w2.Temperature
+
+262. Trips and Users
+    ###Method 1:
+
+WITH comb_trips AS (
+    SELECT
+        t.Status,
+        t.Request_at,
+        u1.Banned AS c_banned,
+        u2.Banned AS d_banned
+    FROM Trips t 
+    LEFT JOIN Users u1 
+        ON (t.Client_Id = u1.Users_Id  AND u1.Role = 'client')
+    LEFT JOIN Users u2 
+        ON (t.Driver_Id = u2.Users_Id  AND u2.Role = 'driver')    
+)
+
+SELECT 
+    Day,
+    Cancellation_Rate AS 'Cancellation Rate'
+FROM (
+    SELECT 
+        Request_at AS Day, 
+        ROUND(
+            (COUNT(
+                CASE 
+                WHEN Status <> 'completed'
+                    AND c_banned = 'No'
+                    AND d_banned = 'No'
+                THEN Status 
+                END) 
+            /
+            COUNT(
+                CASE 
+                WHEN c_banned = 'No' AND d_banned = 'No'
+                THEN Status 
+                END) 
+            ),  
+        2) AS Cancellation_Rate
+    FROM comb_trips 
+    GROUP BY Request_at
+    HAVING 
+        Request_at IN ("2013-10-01", "2013-10-02", "2013-10-03")
+        AND Cancellation_Rate IS NOT NULL
+) crate
+
+###Method 2 Easier Way:
+
+SELECT 
+    t.request_at AS Day, 
+	ROUND(
+        SUM(
+            IF(T.STATUS = 'completed',0,1)
+        )
+        / 
+        COUNT(T.STATUS),
+        2
+	) AS 'Cancellation Rate'
+FROM Trips AS t
+JOIN Users AS u1 ON (t.client_id = u1.users_id AND u1.banned ='No')
+JOIN Users AS u2 ON (t.driver_id = u2.users_id AND u2.banned ='No')
+WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at
