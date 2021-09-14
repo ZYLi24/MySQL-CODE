6c627ebab2497f36d8c8823783bc6eb2a29a573f
Zhenyuan <zhenyuan.li524@gmail.com>
2021-09-14 23:03:33 +0930

    Update LeetCode All.sql

--- LeetCode All.sql
+++ LeetCode All.sql
@@ -228,11 +228,22 @@ FROM Activity a1
 
 SELECT ROUND(
     (
-        (SELECT COUNT(DISTINCT player_id)
-        FROM Activity
-        WHERE (player_id, event_date) IN (SELECT player_id, ADDDATE(MIN(event_date), INTERVAL 1 DAY) FROM Activity GROUP BY player_id))
+        (
+            SELECT COUNT(DISTINCT player_id)
+            FROM Activity
+            WHERE (player_id, event_date) 
+                IN (
+                        SELECT 
+                            player_id, 
+                            ADDDATE(MIN(event_date), INTERVAL 1 DAY) 
+                        FROM Activity 
+                        GROUP BY player_id
+                    )
+        )
         /
-        (SELECT COUNT(DISTINCT player_id)
-        FROM Activity)
+        (
+            SELECT COUNT(DISTINCT player_id)
+            FROM Activity
+        )
     ), 2
 ) AS fraction
